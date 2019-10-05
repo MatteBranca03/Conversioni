cba0a21d4fabf0c4f7c6265fdc8d21860a37b38f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:51:04 +0200

    convrr

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci un numero da convertire");
+            int valute = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(valute, 2);
+            Console.WriteLine($"il corrispettivo numero binario è {binary}");
+            string ottale = Convert.ToString(valute, 8);
+            Console.WriteLine($"il corrispettivo numero ottale è {ottale}");
+            string esadecimale = Convert.ToString(valute, 16);
+            Console.WriteLine($"il corrispettivo numero esadecimale è {esadecimale}");
+            Console.ReadLine();
         }
     }
 }
